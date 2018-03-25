db93c49e33dc93b0c053c720fc966b1f059846f4
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-03-25 11:24:54 +0200

    Add forward jump optimization

--- src/brainfuck.pl
+++ src/brainfuck.pl
@@ -5,7 +5,8 @@ parse(String, Result) :-
 	string(String),
 	string_codes(String, Characters),
 	parse_instructions(Characters, Instructions),
-	optimize_instructions(Instructions, Result).
+	optimize_instructions(Instructions, OptimizedInstructions),
+	optimize_jumps(OptimizedInstructions, Result).
 
 % Ascii codes:
 % . 46
@@ -54,3 +55,29 @@ optimize_instructions([prev(X)|[prev(Y)|Tail]], Result) :-
 	optimize_instructions([prev(Z)|Tail], Result), !.
 optimize_instructions([Head|Tail], [Head|Result]) :-
 	optimize_instructions(Tail, Result), !.
+
+% Optimizes [ ]
+optimize_jumps(In, Out) :-
+	optimize_jumps(In, 0, Out).
+optimize_jumps([], _, []).
+optimize_jumps([open|Tail], Index, [open(TargetIndex)|Result]) :-
+	NewIndex is Index + 1,
+	find_forward_match(NewIndex, Tail, 0, TargetIndex),
+	optimize_jumps(Tail, NewIndex, Result), !.
+optimize_jumps([Head|Tail], Index, [Head|Result]) :-
+	NewIndex is Index + 1,
+	optimize_jumps(Tail, NewIndex, Result), !.
+
+find_forward_match(Index, [close|_], 0, Index).
+find_forward_match(Index, [open|Tail], Counter, TargetIndex) :-
+	NewIndex is Index + 1,
+	NewCounter is Counter + 1,
+	find_forward_match(NewIndex, Tail, NewCounter, TargetIndex), !.
+find_forward_match(Index, [close|Tail], Counter, TargetIndex) :-
+	Counter > 0,
+	NewIndex is Index + 1,
+	NewCounter is Counter - 1,
+	find_forward_match(NewIndex, Tail, NewCounter, TargetIndex), !.
+find_forward_match(Index, [_|Tail], Counter, TargetIndex) :-
+	NewIndex is Index + 1,
+	find_forward_match(NewIndex, Tail, Counter, TargetIndex), !.
