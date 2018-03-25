972a746f35b613b3877126e61175b9e6be041301
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-03-25 11:09:52 +0200

    Remove accumulators

--- src/brainfuck.pl
+++ src/brainfuck.pl
@@ -4,9 +4,8 @@
 parse(String, Result) :-
 	string(String),
 	string_codes(String, Characters),
-	%reverse(Characters, ReversedCharacters),
-	parse_instructions(Characters, [], Instructions),
-	optimize_instructions(Instructions, [], Result).
+	parse_instructions(Characters, Instructions),
+	optimize_instructions(Instructions, Result).
 
 % Ascii codes:
 % . 46
@@ -19,39 +18,39 @@ parse(String, Result) :-
 % ] 93
 
 % Creates the instructions list.
-parse_instructions([], Acc, Acc).
-parse_instructions([46|Tail], Acc, Result) :-
-	parse_instructions(Tail, [print|Acc], Result), !.
-parse_instructions([44|Tail], Acc, Result) :-
-	parse_instructions(Tail, [take|Acc], Result), !.
-parse_instructions([43|Tail], Acc, Result) :-
-	parse_instructions(Tail, [add(1)|Acc], Result), !.
-parse_instructions([45|Tail], Acc, Result) :-
-	parse_instructions(Tail, [sub(1)|Acc], Result), !.
-parse_instructions([62|Tail], Acc, Result) :-
-	parse_instructions(Tail, [next(1)|Acc], Result), !.
-parse_instructions([60|Tail], Acc, Result) :-
-	parse_instructions(Tail, [prev(1)|Acc], Result), !.
-parse_instructions([91|Tail], Acc, Result) :-
-	parse_instructions(Tail, [open|Acc], Result), !.
-parse_instructions([93|Tail], Acc, Result) :-
-	parse_instructions(Tail, [close|Acc], Result), !.
-parse_instructions([_|Tail], Acc, Result) :-
-	parse_instructions(Tail, Acc, Result), !.
+parse_instructions([], []).
+parse_instructions([46|Tail], [print|Result]) :-
+	parse_instructions(Tail, Result), !.
+parse_instructions([44|Tail], [take|Result]) :-
+	parse_instructions(Tail, Result), !.
+parse_instructions([43|Tail], [add(1)|Result]) :-
+	parse_instructions(Tail, Result), !.
+parse_instructions([45|Tail], [sub(1)|Result]) :-
+	parse_instructions(Tail, Result), !.
+parse_instructions([62|Tail], [next(1)|Result]) :-
+	parse_instructions(Tail, Result), !.
+parse_instructions([60|Tail], [prev(1)|Result]) :-
+	parse_instructions(Tail, Result), !.
+parse_instructions([91|Tail], [open|Result]) :-
+	parse_instructions(Tail, Result), !.
+parse_instructions([93|Tail], [close|Result]) :-
+	parse_instructions(Tail, Result), !.
+parse_instructions([_|Tail], Result) :-
+	parse_instructions(Tail, Result), !.
 
 % Optimizes + - > <
-optimize_instructions([], Acc, Acc).
-optimize_instructions([add(X)|[add(Y)|Tail]], Acc, Result) :-
+optimize_instructions([], []).
+optimize_instructions([add(X)|[add(Y)|Tail]], Result) :-
 	Z is X + Y,
-	optimize_instructions([add(Z)|Tail], Acc, Result), !.
-optimize_instructions([sub(X)|[sub(Y)|Tail]], Acc, Result) :-
+	optimize_instructions([add(Z)|Tail], Result), !.
+optimize_instructions([sub(X)|[sub(Y)|Tail]], Result) :-
 	Z is X + Y,
-	optimize_instructions([sub(Z)|Tail], Acc, Result), !.
-optimize_instructions([next(X)|[next(Y)|Tail]], Acc, Result) :-
+	optimize_instructions([sub(Z)|Tail], Result), !.
+optimize_instructions([next(X)|[next(Y)|Tail]], Result) :-
 	Z is X + Y,
-	optimize_instructions([next(Z)|Tail], Acc, Result), !.
-optimize_instructions([prev(X)|[prev(Y)|Tail]], Acc, Result) :-
+	optimize_instructions([next(Z)|Tail], Result), !.
+optimize_instructions([prev(X)|[prev(Y)|Tail]], Result) :-
 	Z is X + Y,
-	optimize_instructions([prev(Z)|Tail], Acc, Result), !.
-optimize_instructions([Head|Tail], Acc, Result) :-
-	optimize_instructions(Tail, [Head|Acc], Result), !.
+	optimize_instructions([prev(Z)|Tail], Result), !.
+optimize_instructions([Head|Tail], [Head|Result]) :-
+	optimize_instructions(Tail, Result), !.
