a51266c31723ee297936ad92cd6c9cbf255b1c07
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-03-25 12:29:38 +0200

    Desugar sub and prev to add and next

--- src/brainfuck.pl
+++ src/brainfuck.pl
@@ -26,11 +26,11 @@ parse_instructions([44|Tail], [take|Result]) :-
 	parse_instructions(Tail, Result), !.
 parse_instructions([43|Tail], [add(1)|Result]) :-
 	parse_instructions(Tail, Result), !.
-parse_instructions([45|Tail], [sub(1)|Result]) :-
+parse_instructions([45|Tail], [add(-1)|Result]) :-
 	parse_instructions(Tail, Result), !.
 parse_instructions([62|Tail], [next(1)|Result]) :-
 	parse_instructions(Tail, Result), !.
-parse_instructions([60|Tail], [prev(1)|Result]) :-
+parse_instructions([60|Tail], [next(-1)|Result]) :-
 	parse_instructions(Tail, Result), !.
 parse_instructions([91|Tail], [open|Result]) :-
 	parse_instructions(Tail, Result), !.
@@ -44,15 +44,9 @@ optimize_instructions([], []).
 optimize_instructions([add(X)|[add(Y)|Tail]], Result) :-
 	Z is X + Y,
 	optimize_instructions([add(Z)|Tail], Result), !.
-optimize_instructions([sub(X)|[sub(Y)|Tail]], Result) :-
-	Z is X + Y,
-	optimize_instructions([sub(Z)|Tail], Result), !.
 optimize_instructions([next(X)|[next(Y)|Tail]], Result) :-
 	Z is X + Y,
 	optimize_instructions([next(Z)|Tail], Result), !.
-optimize_instructions([prev(X)|[prev(Y)|Tail]], Result) :-
-	Z is X + Y,
-	optimize_instructions([prev(Z)|Tail], Result), !.
 optimize_instructions([Head|Tail], [Head|Result]) :-
 	optimize_instructions(Tail, Result), !.
 
